0d6a9bd6046f50efcae36223061dc75a4440574d
erikmyrland <satelitarnantena@gmail.com>
2023-01-02 20:45:46 -0600

    made cool methods static

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -2,11 +2,11 @@ package utils;
 
 public class MainUtils {
 
-    public void coolMethod(){
+    public static void coolMethod(){
 
     }
 
-    public void otherCoolMethods(){
+    public static void otherCoolMethods(){
 
     }
 
